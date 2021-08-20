104b71ffc7b3921fd0c83b04d8453569b487190e
portal-sel <89226629+portal-sel@users.noreply.github.com>
2021-08-20 11:35:20 -0500

    Update script_perfil.js

--- js/script_perfil.js
+++ js/script_perfil.js
@@ -3,6 +3,8 @@ function setup() {
     setName()
 
     docSelector = select('#selectDoc')
+    document.getElementById('txt_name').innerHTML = `NOMBRE DE PROVEEDOR: ${localStorage.getItem('nombre')}`
+    document.getElementById('txt_rfc').innerHTML = `RFC DE PROVEEDOR: ${localStorage.getItem('rfc')}`
 
     for (let index = 1; index < 10; index++) {
         docSelector.option(`Documento ${index}`)
